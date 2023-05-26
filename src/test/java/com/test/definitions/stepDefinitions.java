24287a1b84d4dbdef785d172c0416a16b134a18c
Mahmood Alsaftawi <mahmood.alsaftawi@gmail.com>
2023-05-26 15:50:19 -0400

    Updated Steps Definitions

--- src/test/java/com/test/definitions/stepDefinitions.java
+++ src/test/java/com/test/definitions/stepDefinitions.java
@@ -7,6 +7,7 @@ import io.cucumber.java.en.When;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.junit.Assert;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -18,6 +19,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.By;
 
 import java.time.Duration;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -34,6 +36,7 @@ public class stepDefinitions {
 
 
     @Given("I launch chrome browser")
+
     public void iLaunchChromeBrowser() {
         System.setProperty("webDriver.chrome.driver", "\"D:\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe\"" );
         logger.info("Chrome browser is launched");
@@ -42,6 +45,12 @@ public class stepDefinitions {
     @When("I navigate to amazon.ca")
     public void iNavigateToAmazonCa() {
         driver.get("https://www.amazon.ca/");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        try {
+//            Thread.sleep(5000);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
         logger.info("Navigated to Amazon.ca");
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"nav-logo-sprites\"]")));
         logger.info("Wait for page to full load");
@@ -61,8 +70,11 @@ public class stepDefinitions {
     public void clickOnHamburgerMenu() {
         driver.findElement(By.xpath("//*[@id=\"nav-hamburger-menu\"]")).click();
         logger.info("Click on Hamburger Menu");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div[2]/div/ul[1]")));
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         logger.info("Wait until hamburger menu populates");
     }
 
@@ -73,26 +85,37 @@ public class stepDefinitions {
             driver.navigate().refresh();
             logger.info("Hamburger menu did not open, refreshing");
         }
-        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"hmenu-content\"]/ul[1]/li[10]/a")));
         logger.info("Hamburger menu open successfully, continuing test steps");
     }
 
     @Then("click on Kindle")
     public void clickOnKindle() {
-        WebElement kindle = driver.findElement(By.cssSelector("li:nth-child(10) .hmenu-item"));
-        actions.moveToElement(kindle).click().build().perform();
+        List<WebElement> kindleOptions = driver.findElements(By.className("hmenu-item"));
+/*      WebElement kindle = driver.findElement(By.xpath("//div[@id='hmenu-content']/ul/li[10]/a/div"));*/
+        WebElement kindle = driver.findElement(By.xpath("//div[contains(text(),'Kindle')]"));
+        kindle.click();
+        try {
+            Thread.sleep(1500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
         logger.info("Find and click on Kindle option in hamburger menu");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[3]/div[2]/div/ul[5]/li[2]/div")));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id=\"hmenu-content\"]/ul[5]/li[3]/a")));
         logger.info("Wait for submenu to load properly");
 
     }
 
     @Then("click on Kindle under Kindle E-Readers")
     public void clickOnKindleUnderKindleEReaders() {
-        driver.findElement(By.xpath("//*[@id=\"hmenu-content\"]/ul[5]/li[3]/a")).click();
+        boolean kindleExists = driver.findElements(By.xpath("/html/body/div[3]/div[2]/div/ul[5]/li[3]/a")).size() > 0;
+        if (!kindleExists){
+            driver.findElement(By.xpath("/html/body/div[7]/div[2]/div/ul[4]/li[3]/a")).click();
+            driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/ul[5]/li[3]/a")).click();
+        }
+        else {
+            driver.findElement(By.xpath("/html/body/div[3]/div[2]/div/ul[5]/li[3]/a")).click();
+            driver.findElement(By.xpath("/html/body/div[7]/div[2]/div/ul[4]/li[3]/a")).click();
+        }
         logger.info("Click on Kindle Options under Kindle E-Readers");
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("/html/body/div[4]/div[3]/div[4]/div[12]/div[1]/div[4]/div/div[1]/div/div/div/form/div[1]/div/div/div/div[3]/div/div[14]/div/div/span[2]/span/input")));
         logger.info("Wait for Kindle Page to render");
     }
 
@@ -108,6 +131,7 @@ public class stepDefinitions {
         if(emailorphonefieldisPresent = true){
             System.out.println("User is successfully asked for email or phone number");
         }
+        driver.quit();
     }
 
 
--- src/test/java/testRunner/cucumberRunner.java
+++ src/test/java/testRunner/cucumberRunner.java
@@ -2,6 +2,7 @@ package testRunner;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
@@ -9,6 +10,7 @@ import org.junit.runner.RunWith;
         features = "src/test/resources",
         glue = "com.test.definitions"
 )
+
 public class cucumberRunner {
 
 }
